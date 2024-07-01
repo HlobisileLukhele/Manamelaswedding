fb24e2f55d793694fbb578425fae726207b5a8fa
HlobisileLukhele <n.thwala03@gmail.com>
2024-07-01 00:20:30 -0700

    corrected path name

--- src/main.jsx
+++ src/main.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
         element: <Story />,
       },
       {
-        path: 'Accomodation',
+        path: 'Accommodation',
         element: <Accommodation />,
       },
       {
